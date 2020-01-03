334a4b4bbf1a44ddaf956544ef5e0b109b62f5c6
JGSims <josi3006@hotmail.com>
2020-01-03 13:18:49 -0500

    Cleaned up and made notes in script file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,26 @@
+
+// Defining variables
+
 const headlinesGuardian = [];
 const URLsGuardian = [];
 const headlinesNYT = [];
 const URLsNYT = [];
 
-// Search Button function that calls both AJAX functions
+
+
+// Search Button function that calls both AJAX functions and Street Fighter scene
+
 function searchButton() {
     displayStreetFighterScene();
     var searchTerm = $('#searchBar').val();
     setTimeout(searchGuardian(searchTerm), 1500);
     setTimeout(searchNYT(searchTerm), 1500);
-
-
 }
 
+
+
 // Function to make AJAX call to The Guardian
+
 function searchGuardian(searchTerm) {
 
     let queryURL = 'https://content.guardianapis.com/search?q=' + searchTerm + '&editions?q=uk&api-key=eea751dd-bcde-4212-9e2e-0b0f669651bb'
@@ -26,16 +33,15 @@ function searchGuardian(searchTerm) {
         for (let i = 0; i < 10; i++) {
 
             // For Loop creates 10 results, each including the headline and the URL.
-            // Results are pushed into an array
+            // Results are pushed into arrays (one for headlines, one for URLs)
 
             headlinesGuardian.push(response.response.results[i].webTitle);
             URLsGuardian.push(response.response.results[i].webUrl);
-
-            console.log(response.response.results[i].webTitle);
-            console.log(response.response.results[i].webUrl);
         }
     });
 }
+
+
 
 // Funtion to make AJAX call to New York Times
 
@@ -48,48 +54,57 @@ function searchNYT(searchTerm) {
     }).then(function (response) {
         for (i = 0; i < 10; i++) {
 
+            // For Loop creates 10 results, including both headlines and URLs
+            // Results are pushedinto arrays (one for headlines, one for URLs)
 
             headlinesNYT.push(response.response.docs[i].headline.main);
             URLsNYT.push(response.response.docs[i].web_url);
-
-            // console.log("URL: " + queryNYT);
-            console.log(response.response.docs[i].headline.main);
-            console.log(response.response.docs[i].web_url)
-            // console.log('NYT: ', response[i]);
-            // console.log(queryNYT);
-            // console.log('NYT: ' + response.headline);
         }
-        populateResults();          // Function to populate results is called within the NYT Ajax function
+
+
+        populateResults();          // calls function to populate results from within the NYT Ajax function
     });
 }
 
+
+
 // Function to populate search results on the HTML page
+
 function populateResults() {
+
     for (j = 0; j < headlinesGuardian.length; j++) {
-        //For Loop renders search results arrays by using headline as link title, URL as link source
+        
+        // For Loop renders search results arrays by using headline as link title, URL as link source
+        // Each array index is appended into proper div on HTML page
 
         $('#newsSource2').append('<p><a href=' + URLsGuardian[j] + '>' + headlinesGuardian[j] + '</a></p>');
-
-
     }
+
 
     for (k = 0; k < headlinesNYT.length; k++) {
 
+        // For Loop renders search results arrays by using headline as link title, URL as link source
+        // Each array index is appended into proper div on HTML page
+
         $('#newsSource1').append('<p><a href=' + URLsNYT[k] + '>' + headlinesNYT[k] + '</a></p>');
     }
-
 }
+
+
+
+// This function displays a scene from Street FIghter during search
 
 function displayStreetFighterScene() {
     $('#street-fighter-gif').show();
     setTimeout(function () {
         $('#street-fighter-gif').hide()
     }, 500);
-
 }
 
 
+
 // Function to ensure document is ready before calling functions, and event listener on search button
+
 $(document).ready(function () {
     $('#searchButton').click(searchButton);
     $('#street-fighter-gif').hide();
